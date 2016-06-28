08bf1463f7b64e0e2021e8a3a15da7c5de16b596
oakes <zsoakes@gmail.com>
2016-06-28 12:31:32 -0400

    Upload functionality

--- src/main/java/com/theironyard/services/PhotoRepository.java
+++ src/main/java/com/theironyard/services/PhotoRepository.java
@@ -1,10 +1,12 @@
 package com.theironyard.services;
 
 import com.theironyard.entities.Photo;
+import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by zach on 6/28/16.
  */
 public interface PhotoRepository extends CrudRepository<Photo, Integer> {
+    public Iterable<Photo> findByRecipient(User recipient);
 }
